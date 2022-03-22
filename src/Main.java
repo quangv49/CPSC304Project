c2cff6ced1064945c5a6525d1591c6e246a951e8
Phuong Nguyen <phuongdm.nguyen@gmail.com>
2022-03-21 20:16:44 -0700

    setup project

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ package src;
 
 public class Main {
     public static void main(String args[]) {
+        DatabaseConnectionHandler dbhandler = new DatabaseConnectionHandler();
     }
 }
 
---
+++ src/ui/UI.java
@@ -0,0 +1,18 @@
+package src.ui;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class UI extends JFrame implements ActionListener {
+    JFrame jframe = new JFrame();
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Water Quality Database Management");
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
